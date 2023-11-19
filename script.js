789e2e5ed6dfdc164e484573598e800260570978
dmrado <dmrado@live.ru>
2023-11-19 15:33:56 +0300

    Carousel 1st

---
+++ script.js
@@ -0,0 +1,37 @@
+// for slider.js
+const carousel = document.querySelector('.carousel')
+const arrowBtns = document.querySelectorAll('.wrapper i')
+const firstCardWidth = carousel.querySelector(".card").offsetWidth
+
+let isDragging = false, startX, startScrollLeft
+
+//Add event listeners for the arrow buttons to scrioll carousel left and right
+arrowBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        carousel.scrollLeft += btn.id ==="left" ? -firstCardWidth : firstCardWidth
+    })
+})
+
+const dragStart = (e) => {
+    isDragging = true
+    carousel.classList.add("dragging")
+    //Records the initial cursor and scroll position of the carousel
+    startX = e.pageX
+    startScrollLeft = carousel.scrollLeft
+}
+
+const dragging = (e) => {
+    if(!isDragging) return
+    //Updates the scroll position of the carousel based on the cursor movement
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX)
+}
+
+const dragStop = () => {
+    isDragging = false
+    carousel.classList.remove("dragging")
+}
+
+
+carousel.addEventListener('mousedown', dragStart)
+carousel.addEventListener('mousemove', dragging)
+document.addEventListener('mouseup', dragStop)
